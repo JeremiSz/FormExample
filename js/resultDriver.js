f3289e56d3a1b7c6fee7b9a555930f8ce1661761
Voxilous <57728240+JeremiSz@users.noreply.github.com>
2021-05-21 14:20:03 +0100

    Added total feedback

--- js/resultDriver.js
+++ js/resultDriver.js
@@ -7,9 +7,55 @@
     answers = JSON.parse(strAnswers);
   }
 
-  for(let index = 0; index<answers.length; index++){
-    showResult((index + 1),answers[index]);
+  for(let index = 1; index<=answers.length; index++){
+    showResult(index,answers[index]);
   }
+  //FINAL RESULT
+  {
+    let text;
+    //GET TEXT
+    {
+      let total = 0, textIndex;
+      answers.forEach((value) => total+= value);
+
+      if(total > 339)
+        textIndex = 0;
+      else if(total > 244)
+        textIndex = 1;
+      else if(total > 149)
+        textIndex = 2;
+      else
+        textIndex = 3;
+      console.log(textIndex);
+      text = Array(
+        `Your organisation is evidently committed to inclusion.  Your perspective will be most valuable at the
+        face to face training session as both an advocate and a guide for inclusive fitness service provision. You are
+        highly relevant in your community and hence the organisation is sustainable and will remain so once your current
+        commitment is supported and enhanced over time.`,
+        `Your organisation is making genuine and meaningful efforts in the direction of universability.  However
+         provision is probably patchy and inconsistent, which may cause some confusion among members and potential
+         members. Promotion of inclusive opportunities as an organisational priority has the potential to broaden your
+         relevance to an even greater number of community members.`,
+        `The face to face training session will be of immense value to your organisation. An exciting challenge awaits
+         as you move towards universability. It will take some time but the secret is to keep doing what you are doing
+          well and gradually add other initiatives to make your services more inclusive. The face to face training
+           session will lead to significant learning for your organisation.`,
+        `The face to face training session will be of immense value to your organisation. An exciting challenge awaits
+        as you move towards universability. It will take some time but the secret is to keep doing what you are doing
+        well and gradually add other initiatives to make your services more inclusive. The face to face training session
+        will lead to significant learning for your organisation. `)[textIndex];
+
+
+    }
+    console.log(text);
+    const eleResult = document.createElement("p");
+    eleResult.innerHTML = text;
+    eleResult.className = FEEDBACK_CLASS;
+    document.getElementById("overall").append(eleResult);
+
+  }
+
+
 }
 
 //LISTBOXES
